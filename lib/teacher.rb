12ce8822cfb04a56616d7b3ca83241573298a039
MullaN <admullan@gmail.com>
2020-09-09 10:13:53 -0700

    added get_attendance method, works but has sql queries in it

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -7,6 +7,10 @@ class Teacher < ActiveRecord::Base
     end
 
     def get_attendance(class_name)
-        
+        correct_classes = self.classrooms.select {|classroom| classroom.name == class_name}
+        if correct_classes.length > 0
+            puts "#{class_name} attendance:"
+            correct_classes.each {|classroom| puts "#{classroom.student.name} - " + (classroom.student.check_in_status ? "here" : "absent") }
+        end
     end
 end
