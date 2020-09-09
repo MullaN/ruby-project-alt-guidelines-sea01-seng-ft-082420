949bc8472a917b3aa1e48b9270439c8759db1c3f
MullaN <admullan@gmail.com>
2020-09-08 17:04:08 -0700

    updated teacher method add_student_to_classroom

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,7 +1,7 @@
-class Teacher
+class Teacher < ActiveRecord::Base
     has_many :students, through: :classrooms
 
-    def add_student_to_classroom(student)
-        Classroom.create(student: student, teacher: self)
+    def add_student_to_classroom(student, class_name)
+        Classroom.create(student_id: student.id, teacher_id: self.id, name: class_name)
     end
 end
