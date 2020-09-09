b1d5870ff91b081d62a2c436a3e7b66991e40053
MullaN <admullan@gmail.com>
2020-09-09 13:48:36 -0700

    added motd message to Student check-in

--- lib/student.rb
+++ lib/student.rb
@@ -7,6 +7,10 @@ class Student < ActiveRecord::Base
         if found_class
             self.update(check_in_status: !self.check_in_status)
             puts "Checked " + (self.check_in_status ? "in to" : "out of") + " #{class_name}"
+            if found_class.motd && self.check_in_status
+                puts "Welcome, #{self.name}! #{found_class.teacher.name} will be your teacher. Today's message of the day is:"
+                puts found_class.motd
+            end
         else
             puts "Could not find #{class_name} in your classes."
         end
