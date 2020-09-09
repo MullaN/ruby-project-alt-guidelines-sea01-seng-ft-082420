c5d5c5fa4355c1c73c23dfcc380d06abc7fdb1a9
MullaN <admullan@gmail.com>
2020-09-09 11:49:27 -0700

    changed check_in in Student to check_in_out allowing for one method that covers in and out.

--- lib/student.rb
+++ lib/student.rb
@@ -2,11 +2,11 @@ class Student < ActiveRecord::Base
     has_many :classrooms
     has_many :teachers, through: :classrooms
 
-    def check_in(class_name)
-        found_class =  self.classrooms.find {|classroom| classroom.name == class_room}
+    def check_in_out(class_name)
+        found_class =  self.classrooms.find {|classroom| classroom.name == class_name}
         if found_class
-            self.update(check_in_status: true)
-            puts "Checked in to #{class_name}"
+            self.update(check_in_status: !self.check_in_status)
+            puts "Checked " + (self.check_in_status ? "in to" : "out of") + " #{class_name}"
         else
             puts "Could not find #{class_name} in your classes."
         end
