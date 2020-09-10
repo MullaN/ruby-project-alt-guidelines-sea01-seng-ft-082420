0747113ee655b82e390847aa327a92bb0f27b497
kendallstephens <kendoll.stephens@gmail.com>
2020-09-10 11:22:12 -0700

    updated methods cli methods

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -29,23 +29,39 @@ end
 end
    
 
-   # def teacher_welcome
-   #     system("clear")
-   #     choices = %w(view_attendance change_welcome_message)
-   #     user_input = PROMPT.select("Would you like to view your class attendance or change your welcome message?", choices)
-   #     if user_input == "view_attendance"
-   #       class_attendance
-   #     elsif user_input == "change_welcome_message"
-   #       change_welcome_message
-   #     end
-   # end
+   def teacher_welcome
+       system("clear")
+       choices = %w(view_attendance change_welcome_message)
+       user_input = PROMPT.select("Would you like to view your class attendance or change your welcome message?", choices)
+       if user_input == "view_attendance"
+         view_attendance
+       elsif user_input == "change_welcome_message"
+         change_welcome_message
+       end
+   end
+
+   def view_attendance
+      puts "Here is your class attendance:"
+   end
+
+   def change_welcome_message
+      puts "Please update your welcome message:"
+   end
     
 
    def student_prompt
       system("clear")
       user_input = PROMPT.ask("Please enter your name.")
       puts "Welcome #{user_input}!"
+      sleep 2
+      choices = %w(science art math language arts music)
+      user_input = PROMPT.multi_select("Please select the class you will be attending?", choices)
    end
+
+
+
+  
+
 
    
    
