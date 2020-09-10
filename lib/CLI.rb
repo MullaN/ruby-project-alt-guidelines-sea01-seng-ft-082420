609e860e905fd92cbd49c150761e856a271bc233
kendallstephens <kendoll.stephens@gmail.com>
2020-09-10 10:35:31 -0700

    updated teacher_prompt

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -1,7 +1,7 @@
 require "tty-prompt"
 require 'pry'
 
-class CommandLineInterface
+class CommandLineInterface 
     def intro
       system("clear")
       choices = %w(student teacher)
@@ -18,20 +18,33 @@ end
    def teacher_prompt
       system("clear")
       user_input = PROMPT.ask("Please enter your name")
-      puts "Welcome, would you like to view your class attendance or change your welcome message?"
+      teacher = Teacher.find_by(name: user_input)
+      if teacher 
+       puts "Welcome, #{user_input}!"
+       sleep 2
+       teacher_welcome
+      else 
+       puts "Sorry, you are not a registered teacher at this location."
    end
+end
    
 
    # def teacher_welcome
-   #    system("clear")
-   #    choices = %w(view_attendance change_welcome_message)
-   #    user_input = PROMPT.select("Would you like to view your class attendance or change your welcome message?")
+   #     system("clear")
+   #     choices = %w(view_attendance change_welcome_message)
+   #     user_input = PROMPT.select("Would you like to view your class attendance or change your welcome message?", choices)
+   #     if user_input == "view_attendance"
+   #       class_attendance
+   #     elsif user_input == "change_welcome_message"
+   #       change_welcome_message
+   #     end
    # end
+    
 
    def student_prompt
       system("clear")
-      user_input = PROMPT.ask("Please enter your name and the class you will be attending.")
-      puts "Welcome, your teacher will be and your assigment for the day is"
+      user_input = PROMPT.ask("Please enter your name.")
+      puts "Welcome #{user_input}!"
    end
 
    
